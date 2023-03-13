976de07646b3e0aba553086451f377e25b6e3497
Mafalda Aires <116459746+mafaldaaires@users.noreply.github.com>
2023-03-13 16:41:20 +0000

    Add files via upload

--- Board.py
+++ Board.py
@@ -1,95 +1,159 @@
+from copy import deepcopy
+
 ROWS = 4
 COLS = 4
-class Board:
 
-    def __init__(self,matrix,blank):
-        self.board=matrix
-        self.blank=blank #[x,y] vetor com a posiçao do quadrado branco
-        self.goal=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
-
+def no_tuplo(n):                          
+    m=n.brd.board
+    l=correspond_list(m)
+    t=tuple(l)
+    return t
     
-    def __repr__(self): #semelhante a __str___
+def list_to_matrix(list):
+    matrix=[[0 for l in range(4)] for l in range(4)]
+    k=0
+    for i in range(4):
+        for j in range(4):
+            matrix[i][j]=list[k]
+            k+=1
+    return matrix
+
+def inversions(lista):
+    inv = 0
+    for i in range(len(lista)):
+        for j in range(i+1,len(lista)):
+            if ((lista[i]>lista[j]) and (lista[i])!=0 and (lista[j])!=0):
+                inv += 1
+    return inv
+
+def correspond_list(matriz):
+    lista = [item for sublist in matriz for item in sublist]
+    return lista
+
+
+#board
+class Board:
+   
+    def __init__(self,matrix,goal):
+        self.goal=goal
+        self.board=deepcopy(matrix)
+        self.blank = self.find_blank()#[x,y]
+
+    def __repr__(self): 
         k=0
         for i in self.board:
+            print()
+            print("+----+----+----+----+")
             for j in i:
-                print(j, end = " ")
+                print("| {:2d} ".format(j), end="")
+            print("|", end="")
             if(k<3):
-                print()
-            k += 1
-        return "" #funçao repr obriga a retornar algo
-        
-    def blank_position(self):
-        return self.blank
+                k += 1
+        print()
+        print("+----+----+----+----+")
+        return "" 
+    
+    
+    def find_blank(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board[row][col] == 0:
+                    return [row, col]
 
-    def finished(self):
-        pass
-
-    #de matriz para lista
-    def correspond_list(self, matriz):
-        list = [item for sublist in matriz for item in sublist]
-        return list
-
-    #funçao pa contar inverçoes N de uma lista correspontente configuraçao da matriz
-    def inversions(self, list):
-        inv = 0
-        for i in range(len(list)):
-            for j in range(i+1,len(list)):
-                if (list[i]>list[j] and list[i]!=0 and list[j]!=0):
-                    inv += 1
-        return inv
-
-    #tendo em conta que o tabuleiro terá sempre tamanho par
-    def possible(self):
-        lista=self.correspond_list(self.board)    # lista que corresponde ao self.board
-        n_inv=self.inversions(lista)              # número de inversões
-        i_blank=self.blank[0]                     # indice da linha do branco
-        if ((n_inv%2)!=(i_blank%2)):
+    def isSolution(self):
+        if (correspond_list(self.board)==self.goal):
             return True
         return False
 
-    #comando, mover branco para cima
+    def possible(self):
+        lista=correspond_list(self.board)    
+        ninv=inversions(lista)              
+        iblank=self.blank[0]                     
+        if ((ninv%2)!=(iblank%2)):
+            return True
+        return False
+
+#comandos:
     def up(self):
         if (self.blank[0]>0 and self.blank[0]<=3):
-            upper=self.board[self.blank[0]-1][self.blank[1]] #upper é uma variavel aux que guarda o valor a cima do blank
+            upper=self.board[self.blank[0]-1][self.blank[1]] 
             self.board[self.blank[0]-1][self.blank[1]]=0
-            self.board[self.blank[0]][self.blank[1]]=upper #neste ponto a troca ja aconteceu
-            self.blank[0]-=1 #atualizar localizaçao do blank
-        else: 
-            return 
+            self.board[self.blank[0]][self.blank[1]]=upper 
+            self.blank[0]-=1 
+            return True
+        return False
 
-    #comando, mover branco para baixo
     def down(self):
         if (self.blank[0]<3 and self.blank[0]>=0):
-            lower=self.board[self.blank[0]+1][self.blank[1]] #lower é uma variavel aux que guarda o valor a baixo do blank
+            lower=self.board[self.blank[0]+1][self.blank[1]] 
             self.board[self.blank[0]+1][self.blank[1]]=0
-            self.board[self.blank[0]][self.blank[1]]=lower #neste ponto a troca ja aconteceu
-            self.blank[0]+=1 #atualizar localizaçao do blank
-        else: 
-            return 
+            self.board[self.blank[0]][self.blank[1]]=lower
+            self.blank[0]+=1 
+            return True
+        return False
     
-    #comando, mover branco para a direita 
     def right(self):
         if (self.blank[1]<3 and self.blank[1]>=0):
-            thenext=self.board[self.blank[0]][self.blank[1]+1] #thenext é uma variavel aux que guarda o valor a direita do blank
+            thenext=self.board[self.blank[0]][self.blank[1]+1] 
             self.board[self.blank[0]][self.blank[1]+1]=0
-            self.board[self.blank[0]][self.blank[1]]=thenext #neste ponto a troca ja aconteceu
-            self.blank[1]+=1 #atualizar localizaçao do blank
-        else: 
-            return 
+            self.board[self.blank[0]][self.blank[1]]=thenext 
+            self.blank[1]+=1 
+            return True        
+        return False
     
-    #comando, mover branco para a esquerda
     def left(self):
         if (self.blank[1]>0 and self.blank[1]<=3):
-            theprev=self.board[self.blank[0]][self.blank[1]-1] #thenext é uma variavel aux que guarda o valor a direita do blank
+            theprev=self.board[self.blank[0]][self.blank[1]-1] 
             self.board[self.blank[0]][self.blank[1]-1]=0
-            self.board[self.blank[0]][self.blank[1]]=theprev #neste ponto a troca ja aconteceu
-            self.blank[1]-=1 #atualizar localizaçao do blank
-        else: 
-            return 
+            self.board[self.blank[0]][self.blank[1]]=theprev
+            self.blank[1]-=1 
+            return True
+        return False
     
+    def expand(self,str):
+        jogo = deepcopy(self)
+        
+        if str=="left":
+            val=jogo.left()
+            if val==False:
+                 return False
 
-    #passar lista para matriz
-    #def matriz(self):
-    #    size = 4
-    #    matrix = [self.goal[i:i+size] for i in range(0, len(self.goal), size)]
-    #    return matrix
+        elif str=="right":
+            val=jogo.right()
+            if val==False:
+                 return False
+
+        elif str=="up":
+            val=jogo.up()
+            if val==False:
+                 return False
+
+        elif str=="down":
+            val=jogo.down()
+            if val==False:
+                 return False
+
+        return jogo
+
+#heuristicas
+    def out_of_place(self):
+        soma = 0
+        lista = correspond_list(self.board)
+        for i in range(len(lista)):
+            if lista[i]!=self.goal[i] and lista[i]!=0:
+                soma += 1
+        return soma
+    
+    def manhattan_distance(self):
+        distancia = 0
+        final = list_to_matrix(self.goal)
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j]!=0 and self.board[i][j] != final[i][j]:
+                    for k in range(len(final)):
+                        if self.board[i][j] in final[k]:
+                            distancia += abs(k - i) + abs(final[k].index(self.board[i][j]) - j)
+        return distancia
+
+    
+        
